abde3cce753cab29af2a8194eac81ca0f987947a
Nick <ngaow29@gmail.com>
2025-02-06 17:45:22 -0500

    Built the router class. The only thing currently is that even though if one doesn't match, i dont see where it send the 404 responses / error text print

--- util/router.py
+++ util/router.py
@@ -1,10 +1,34 @@
+from util.request import Request
+from util.response import Response
+
+
 class Router:
 
     def __init__(self):
-        pass
+        # store different routes where each info of a route will be store as a key-value pair in dict [{}, {}, {}]
+        self.routes = []
 
     def add_route(self, method, path, action, exact_path=False):
-        pass
+        self.routes.append({"method": method, "path": path, "action": action, "exact_path": exact_path})
 
     def route_request(self, request, handler):
-        pass
+        # get the method & path from the req first
+        request_method = request.method
+        request_path = request.path
+
+        #S
+        for route in self.routes:
+            route_method = route["method"]
+            route_path = route["path"]
+            if route_method == request_method and route_path == request_path:
+                if route["exact_path"]:
+                    route["action"](request, handler)
+                    return
+                else:
+                    if request_path.startswith(route_path):
+                        route["action"](request, handler)
+                        return
+
+        #No match occurs
+        res = Response().set_status("404", "404 Not Found")
+        handler.request.sendall(res.to_data())
