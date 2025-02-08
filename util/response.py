8789404790fe1b47a42093a0076ead124c5356ae
Nick <ngaow29@gmail.com>
2025-02-07 23:33:32 -0500

    does the test case cause autograder to fail?

--- util/response.py
+++ util/response.py
@@ -82,8 +82,10 @@ def test_w_multiple_cookies():
     res = Response()
     dict = {"cookie1":"value1", "cookie2":"value2", "cookie3":"value3"}
     res.cookies(dict)
-    expected = b'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length: 0\r\nSet-Cookie: cookie1=value1; Max-Age=3600; Secure; HttpOnly\r\nSet-Cookie: cookie2=value2; Max-Age=3600; Secure; HttpOnly\r\nSet-Cookie: cookie3=value3; Max-Age=3600; Secure; HttpOnly\r\n\r\n'
+    res.bytes(b'hello')
+    expected = b'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length: 5\r\nX-Content-Type-Options: nosniff\r\nSet-Cookie: cookie1=value1; Max-Age=3600; Secure; HttpOnly\r\nSet-Cookie: cookie2=value2; Max-Age=3600; Secure; HttpOnly\r\nSet-Cookie: cookie3=value3; Max-Age=3600; Secure; HttpOnly\r\n\r\nhello'
     actual = res.to_data()
+    print(actual)
 
 if __name__ == '__main__':
     #test1()
