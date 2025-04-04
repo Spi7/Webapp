05b69684694f5d47603b2cabb553282e2ee46438
Nick <ngaow29@gmail.com>
2025-04-03 23:17:31 -0400

    drawing board, user active list implemented.
    Drawing board --> each stroke will be save in database
    
    Active list --> new user connect and disconnect will be broadcast to all users.

--- util/ws_handshake.py
+++ util/ws_handshake.py
@@ -1,7 +1,8 @@
 from util.response import Response
 from util.websockets import compute_accept, parse_ws_frame, generate_ws_frame
 from util.wsFrame import wsFrame
-from util.database import user_collection
+from util.database import user_collection, drawing_collection
+from util.drawingboard import store_drawing, send_all_drawings, broadcast_active_users
 
 import hashlib
 import json
@@ -34,13 +35,18 @@ def handle_ws_connection(request, handler):
     res.headers({"Sec-WebSocket-Accept": accept_response})
     handler.request.sendall(res.to_data())
 
+    send_all_drawings(active_connections[username]) #send all the drawing strokes to this user who's connected
+    broadcast_active_users(active_connections) #broadcast new user connected
+
     buffer = b""
     while True:
         #receive first chunk of 2048 bytes
         received_data = handler.request.recv(2048)
-        # print("======== Received Data for ws =======")
-        # print(received_data)
-        # print("======== End Data =======")
+        print("======== Received Data for ws =======")
+        print(received_data)
+        print("======== End Data =======")
+        if len(received_data) < 2: #initial message is causing error
+            break
 
         buffer += received_data
 
@@ -52,20 +58,29 @@ def handle_ws_connection(request, handler):
         while len(buffer) < full_frame_length:
             buffer += handler.request.recv(2048)
 
-        whole_frame = parse_ws_frame(buffer)
+        this_frame_bytes = buffer[:full_frame_length]
+        buffer = buffer[full_frame_length:]
+        whole_frame = parse_ws_frame(this_frame_bytes)
+
+        if whole_frame.opcode == 1:
+            json_payload = json.loads(whole_frame.payload.decode('utf-8'))
+            message_type = json_payload['messageType']
+
+            if message_type == "echo_client":
+                res_payload = json.dumps({"messageType": "echo_server","text":json_payload["text"]}).encode('utf-8')
+                res_frame = generate_ws_frame(res_payload)
+                handler.request.sendall(res_frame)
+            elif message_type == "drawing":
+                store_drawing(json_payload, active_connections)
 
         #disconnection
-        if whole_frame.opcode == 8:
+        elif whole_frame.opcode == 8:
             del active_connections[username]
-
             close_frame = generate_ws_frame(payload=b"")
             handler.request.sendall(close_frame)
-
             handler.request.close()
+
+            broadcast_active_users(active_connections) #broadcast the user disconnected
             return
 
 
-
-        pass
-
-    # buffer = buffer[full_frame_length:]
